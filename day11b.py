0b9ccf5de956825de2e24b83fce40895cff72802
ncasaux <nicolas.casaux@outlook.com>
2023-12-12 22:24:04 +0100

    added day11b

--- day11b.py
+++ day11b.py
@@ -1,7 +1,16 @@
 import aocd
 
 f = aocd.get_data(day=11, year=2023)
-
+# f = """...#......
+# .......#..
+# #.........
+# ..........
+# ......#...
+# .#........
+# .........#
+# ..........
+# .......#..
+# #...#....."""
 rowIndex = colIndex = None
 gs = []
 distances = []
@@ -21,7 +30,7 @@ for g in range(len(gs)):
     for h in range(g + 1, len(gs)):
         rowIntersect = emptyRow.intersection(range(min(gs[g][0], gs[h][0]), max(gs[g][0], gs[h][0])))
         colIntersect = emptyCol.intersection(range(min(gs[g][1], gs[h][1]), max(gs[g][1], gs[h][1])))
-        dist = abs(gs[h][0] - gs[g][0]) + len(rowIntersect) + abs(gs[h][1] - gs[g][1]) + len(colIntersect)
+        dist = abs(gs[h][0] - gs[g][0]) + 999999 * len(rowIntersect) + abs(gs[h][1] - gs[g][1]) + 999999 * len(colIntersect)
         distances.append(dist)
 
 print(f"Total distance is {sum(distances)}")
